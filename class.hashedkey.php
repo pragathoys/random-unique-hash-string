60103dcf057efaf35a57dec45604203160213d9b
Marountas John <i.marountas@gmail.com>
2022-06-17 17:17:07 +0300

    Create class.hashedkey.php

---
+++ class.hashedkey.php
@@ -0,0 +1,15 @@
+class Hashedkey{
+
+        protected int $size;
+
+        function __construct($size = 8){
+                $this->size = $size;
+        }
+
+        public function get_hashed_key(){
+                $bytes = random_bytes( $this->size );
+                $result = bin2hex($bytes);
+                $formated = substr($result,0,4) . "-" . substr($result,4,4) . "-" . substr($result,8,4). "-" . substr($result,12,4);  
+                return strtoupper( $formated );
+        }
+}
