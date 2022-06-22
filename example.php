f7ba1c415689a34c3d35cd6e7e6c3af862977184
Marountas John <i.marountas@gmail.com>
2022-06-22 10:48:54 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,8 @@
 <?php
-include "class..php";
+include "class.hashedkey.php";
+
+// Create a new instance of the class and expect to create a string with length of 16 characters
 $hashedClass = new Hashedkey(8);
+
+// Get a new hashed string
 echo $hashedClass->get_hashed_key();
